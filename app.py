e29c7fc864f5dbe7ba79da28ee0359fc207bfa90
gentom <gento.m.developer@gmail.com>
2017-11-03 12:28:49 +0900

    commit

--- app.py
+++ app.py
@@ -3,7 +3,18 @@
 from gensim.models import word2vec
 import MeCab
 import sys
+import networkx
 
-# Model Loading
 
-# Word2Vec
+class Network:
+    def __init__(self, model):
+        self.model = model
+
+class Game:
+    def __init__(self, model):
+        self.model = model
+        self.vocb = model.vocab.kyes()
+
+
+if __name__ == "__main__":
+    model = word2vec.Word2Vec.load('./model/jp_wiki')
